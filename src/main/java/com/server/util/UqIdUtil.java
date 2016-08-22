890d9934f5ab53b00b43ccc78bb9ddbb3fe74998
nullzZ <maleivip1988@163.com>
2016-08-22 11:03:10 +0800

    UqIdUtil.java

--- src/main/java/com/server/util/UqIdUtil.java
+++ src/main/java/com/server/util/UqIdUtil.java
@@ -2,15 +2,15 @@ package com.server.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author nullzZ
  */
 public class UqIdUtil {
 
-    private static AtomicInteger count = new AtomicInteger();
+    private static int count = 0;
     private static int max = 10000;
+    private static Object lock = new Object();
 
     /**
      * 生成id---年(2)+月(2)+日(2)+时(2)+分(2)+秒(2)=12 + 4位自增</br>
@@ -23,11 +23,13 @@ public class UqIdUtil {
     public static long buildUqId() {
 	StringBuilder key = new StringBuilder();
 	key.append(new SimpleDateFormat("yyMMddHHmmss").format(new Date()).toString());
-	int num = count.getAndIncrement();
-	String s = String.format("%04d", num);
-	key.append(s);
-	if (count.get() >= max) {
-	    count.set(0);
+	synchronized (lock) {
+	    String s = String.format("%04d", count);
+	    key.append(s);
+	    if (count >= max) {
+		count = 0;
+	    }
+	    count++;
 	}
 	return Long.parseLong(key.toString());
     }
